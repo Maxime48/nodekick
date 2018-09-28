4af7df92948cb0b31c40a0ae5c22b98a1662ac1f
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2018-09-28 11:42:48 +0200

    Update achievements.js

--- public/app/achievements.js
+++ public/app/achievements.js
@@ -1,40 +1,40 @@
 (function() {
   var notifications = {
     "deathfromabove": {
-      me: { message: "Bird of Prey", sound: "deathfromabove-me" },
-      them: { message: "Death from Above", sound: "deathfromabove-them" }
+      me: { message: "Oiseau de proie", sound: "deathfromabove-me" },
+      them: { message: "Mort d'en haut", sound: "deathfromabove-them" }
     },
     "suicide": {
       me: { message: "", sound: "" },
-      them: { message: "Idiot", sound: "suicide-them" }
+      them: { message: "Mort comme une merde", sound: "suicide-them" }
     },
     "counter": {
-      me: { message: "Counter", sound: "counter-me" },
-      them: { message: "Denied", sound: "counter-them" }
+      me: { message: "Conteur", sound: "counter-me" },
+      them: { message: "Refusé", sound: "counter-them" }
     },
     "headshot": {
       me: { message: "Headshot", sound: "headshot-me" },
-      them: { message: "Kicked in the Face", sound: "headshot-them" }
+      them: { message: "Frapé en pleine façe", sound: "headshot-them" }
     },
     "killstreak-3": {
-      me: { message: "Killing Streak", sound: "killstreak-3" },
-      them: { message: "Lamb to the Slaughter", sound: "killstreak-them" }
+      me: { message: "Série de meutres", sound: "killstreak-3" },
+      them: { message: "Agneau à l'abattoir", sound: "killstreak-them" }
     },
     "killstreak-6": {
-      me: { message: "Rampage", sound: "killstreak-6" },
-      them: { message: "Lamb to the Slaughter", sound: "killstreak-them" }
+      me: { message: "Déchaînement", sound: "killstreak-6" },
+      them: { message: "Agneau à l'abattoir", sound: "killstreak-them" }
     },
     "killstreak-9": {
-      me: { message: "Dominating", sound: "killstreak-9" },
-      them: { message: "Lamb to the Slaughter", sound: "killstreak-them" }
+      me: { message: "Domination", sound: "killstreak-9" },
+      them: { message: "Agneau à l'abattoir", sound: "killstreak-them" }
     },
     "killstreak-12": {
-      me: { message: "Unstoppable", sound: "killstreak-12" },
-      them: { message: "Lamb to the Slaughter", sound: "killstreak-them" }
+      me: { message: "Instopable", sound: "killstreak-12" },
+      them: { message: "Agneau à l'abattoir", sound: "killstreak-them" }
     },
     "killstreak-15": {
-      me: { message: "Godlike", sound: "killstreak-15" },
-      them: { message: "Lamb to the Slaughter", sound: "killstreak-them" }
+      me: { message: "Comme Un Dieu", sound: "killstreak-15" },
+      them: { message: "Agneau à l'abattoir", sound: "killstreak-them" }
     },
   };
 
